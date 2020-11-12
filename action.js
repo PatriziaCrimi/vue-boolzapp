115ee430bf03586d5f73ce0d00adfae2e2b0868e
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-11-12 17:34:07 +0100

    Basic style & structure added & printing contacts on screen with "v-for"

---
+++ action.js
@@ -0,0 +1,63 @@
+let app = new Vue({
+  el: '#root',
+  data: {
+    contacts_list: [
+      {
+        name: 'Albert Camus',
+        avatar: 'img/camus.jpg',
+        visible: true,
+        messages_list: [
+          {
+            date: '10/01/2020 15:30:55',
+            message: 'First message',
+            status: 'sent',
+          },
+          {
+            date: '10/01/2020 15:30:55',
+            message: 'Second message',
+            status: 'sent',
+          },
+        ],
+      },
+      {
+        name: 'Immanuel Kant',
+        avatar: 'img/kant.jpg',
+        visible: true,
+        messages_list: [
+          {
+            date: '10/01/2020 15:30:55',
+            message: 'Some text',
+            status: 'sent',
+          },
+        ],
+      },
+      {
+        name: 'Søren Kierkegaard',
+        avatar: 'img/kierkegaard.jpg',
+        visible: true,
+        messages_list: [
+          {
+            date: '10/01/2020 15:30:55',
+            message: 'Some text',
+            status: 'sent',
+          },
+        ],
+      },
+      {
+        name: 'Friedrich Nietzsche',
+        avatar: 'img/nietzsche.jpg',
+        visible: true,
+        messages_list: [
+          {
+            date: '10/01/2020 15:30:55',
+            message: 'Some text',
+            status: 'sent',
+          },
+        ],
+      },
+    ]
+  },  // Closing data
+  methods: {
+
+  },  // Closing methods
+});
