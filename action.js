558b0eea32d62e3b7f6d796ac3843ffd8c0c3095
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-11-12 20:59:07 +0100

    Aside completed with HTML, CSS and JS

--- action.js
+++ action.js
@@ -55,6 +55,57 @@ let app = new Vue({
           },
         ],
       },
+      {
+        name: 'Plato',
+        avatar: 'img/plato.jpg',
+        visible: true,
+        messages_list: [
+          {
+            date: '10/01/2020 15:30:55',
+            message: 'First message',
+            status: 'sent',
+          },
+          {
+            date: '10/01/2020 15:30:55',
+            message: 'Second message',
+            status: 'sent',
+          },
+        ],
+      },
+      {
+        name: 'Socrate',
+        avatar: 'img/socrate.jpg',
+        visible: true,
+        messages_list: [
+          {
+            date: '10/01/2020 15:30:55',
+            message: 'First message',
+            status: 'sent',
+          },
+          {
+            date: '10/01/2020 15:30:55',
+            message: 'Second message',
+            status: 'sent',
+          },
+        ],
+      },
+      {
+        name: 'Baruch Spinoza',
+        avatar: 'img/spinoza.jpg',
+        visible: true,
+        messages_list: [
+          {
+            date: '10/01/2020 15:30:55',
+            message: 'First message',
+            status: 'sent',
+          },
+          {
+            date: '10/01/2020 15:30:55',
+            message: 'Second message',
+            status: 'sent',
+          },
+        ],
+      },
     ]
   },  // Closing data
   methods: {
