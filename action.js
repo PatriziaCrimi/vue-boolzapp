5c9a484e9243b82c5e6b77bc241c4c92b540d8a0
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-11-13 16:35:57 +0100

    Milestone 2 completed (bug to be fixed for sent messages)

--- action.js
+++ action.js
@@ -1,8 +1,9 @@
 let app = new Vue({
   el: '#root',
   data: {
+    index_chat: 0,
     user: {
-      name: 'Ratman',
+      name: 'Rat-Man',
       avatar: 'img/ratman.jpg',
       messages_list: [
         {
@@ -46,29 +47,12 @@ let app = new Vue({
           {
             date: '***data da rendere dinamica***',
             message: 'No man ever steps in the same river twice, for it\'s not the same river and he\'s not the same man.',
-            status: 'sent',
+            status: 'received',
           },
           {
             date: '***data da rendere dinamica***',
-            message: 'There is nothing permanente except change.',
-            status: 'sent',
-          },
-        ],
-      },
-      {
-        name: 'Albert Camus',
-        avatar: 'img/camus.jpg',
-        visible: true,
-        messages_list: [
-          {
-            date: '***data da rendere dinamica***',
-            message: 'You will never be happy if you continue to search for what happiness consists of. You will never live if you are looking for the meaning of life.',
-            status: 'sent',
-          },
-          {
-            date: '***data da rendere dinamica***',
-            message: 'The only way to deal with an unfree world is to become so absolutely free that your very existence is an act of rebellion.',
-            status: 'sent',
+            message: 'There is nothing permanent except change.',
+            status: 'received',
           },
         ],
       },
@@ -80,7 +64,34 @@ let app = new Vue({
           {
             date: '***data da rendere dinamica***',
             message: 'Without music, life would be a mistake.',
-            status: 'sent',
+            status: 'received',
+          },
+          {
+            date: '***data da rendere dinamica***',
+            message: 'And those who were seen dancing were thought to be insane by those who could not hear the music.',
+            status: 'received',
+          },
+          {
+            date: '***data da rendere dinamica***',
+            message: 'You must have chaos within you to give birth to a dancing star.',
+            status: 'received',
+          },
+        ],
+      },
+      {
+        name: 'Albert Camus',
+        avatar: 'img/camus.jpg',
+        visible: true,
+        messages_list: [
+          {
+            date: '***data da rendere dinamica***',
+            message: 'You will never be happy if you continue to search for what happiness consists of. You will never live if you are looking for the meaning of life.',
+            status: 'received',
+          },
+          {
+            date: '***data da rendere dinamica***',
+            message: 'The only way to deal with an unfree world is to become so absolutely free that your very existence is an act of rebellion.',
+            status: 'received',
           },
         ],
       },
@@ -92,22 +103,22 @@ let app = new Vue({
           {
             date: '***data da rendere dinamica***',
             message: 'There are two ways to be fooled. One is to believe what isn\'t true, the other is to refuse to accept what is true.',
-            status: 'sent',
+            status: 'received',
           },
           {
             date: '***data da rendere dinamica***',
             message: 'Life is not a problem to be solved, but a reality to be experienced.',
-            status: 'sent',
+            status: 'received',
           },
           {
             date: '***data da rendere dinamica***',
             message: 'What labels me, negates me.',
-            status: 'sent',
+            status: 'received',
           },
           {
             date: '***data da rendere dinamica***',
             message: 'Life can only be understood backwards; but it must be lived forwards.',
-            status: 'sent',
+            status: 'received',
           },
         ],
       },
@@ -119,12 +130,12 @@ let app = new Vue({
           {
             date: '***data da rendere dinamica***',
             message: 'Wise men speak because they have something to say; fools because they have to say something.',
-            status: 'sent',
+            status: 'received',
           },
           {
             date: '***data da rendere dinamica***',
             message: 'Courage is knowing what not to fear.',
-            status: 'sent',
+            status: 'received',
           },
         ],
       },
@@ -136,17 +147,17 @@ let app = new Vue({
           {
             date: '***data da rendere dinamica***',
             message: 'By all means marry; if you get a good wife, you’ll become happy; if you get a bad one, you’ll become a philosopher.',
-            status: 'sent',
+            status: 'received',
           },
           {
             date: '***data da rendere dinamica***',
             message: 'There is only one good, knowledge, and one evil, ignorance.',
-            status: 'sent',
+            status: 'received',
           },
           {
-            date: '10/01/2020 15:30:55',
+            date: '***data da rendere dinamica***',
             message: 'The only true wisdom is in knowing you know nothing.',
-            status: 'sent',
+            status: 'received',
           },
         ],
       },
@@ -158,22 +169,22 @@ let app = new Vue({
           {
             date: '***data da rendere dinamica***',
             message: 'If you want the present to be different from the past, study the past.',
-            status: 'sent',
+            status: 'received',
           },
           {
             date: '***data da rendere dinamica***',
             message: 'The more clearly you understand yourself and your emotions, the more you become a lover of what is.',
-            status: 'sent',
+            status: 'received',
           },
           {
             date: '***data da rendere dinamica***',
             message: 'The highest activity a human being can attain is learning for understanding, because to understand is to be free.',
-            status: 'sent',
+            status: 'received',
           },
           {
             date: '***data da rendere dinamica***',
             message: 'Be not astonished at new ideas; for it is well known to you that a thing does not therefore cease to be true because it is not accepted by many.',
-            status: 'sent',
+            status: 'received',
           },
         ],
       },
@@ -185,18 +196,20 @@ let app = new Vue({
           {
             date: '***data da rendere dinamica***',
             message: 'Space and time are the framework within which the mind is constrained to construct its experience of reality.',
-            status: 'sent',
+            status: 'received',
           },
           {
             date: '***data da rendere dinamica***',
             message: 'We are not rich by what we possess but by what we can do without.',
-            status: 'sent',
+            status: 'received',
           },
         ],
       },
     ]
   },  // Closing data
   methods: {
-
+    showChat: function(index_contact) {
+      this.index_chat = index_contact;
+    },
   },  // Closing methods
 });
